2366bf6182ecbe963939df4a7f62318ff2f41815
Ethan Zhang <ezhang3333@gmail.com>
2025-11-18 12:36:59 -0600

    more qb cleaning work

--- data_cleaners/positions/qb_cleaner.py
+++ data_cleaners/positions/qb_cleaner.py
@@ -10,3 +10,11 @@ class QBCleaner:
         }
 
 # add trendy stats like past 5 weeks usage and target share or like past 3 weeks, decide the number of weeks later        
+"""
+ THESE ARE INDENTIFIERS: 'team', 'position', 'full_name', 'gsis_id', 'week'
+NEED TO ENCODE THESE: 'home_team', 'away_team'
+'height', 'weight', 'years_exp', 'draft_number', 'total', 'spread_line', 'roof', 'surface', 'temp', 'wind',
+'avg_time_to_throw', 'avg_intended_air_yards', 'aggressiveness', 'avg_air_yards_to_sticks',
+'completion_percentage_above_expectation', 'max_air_distance', 'pass_attempt', 'pass_air_yards', 'pass_yards_gained
+'pass_touchdown', 'rush_touchdown', 'pass_interception', 'pass_touchdown_diff', ''
+"""
